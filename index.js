83bd31c739f8f26b71f031c063819329b2ba6cbd
abelzer09 <aaronrbelzer@gmail.com>
2022-04-19 16:12:16 -0500

    made class employee, made subclass Manager

--- index.js
+++ index.js
@@ -1,3 +1,43 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const path = require('path');
+
+
+class Employee {
+    constructor(name, id, email){
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    }
+    
+    getName(){
+
+    }
+
+    getId(){
+
+    }
+
+    getEmail(){
+
+    }
+
+    getRole(){
+        return "Employee"
+    }
+}
+
+class Manager extends Employee {
+    constructor(name, officeNumber){
+        super(name, id, email);
+        this.officeNumber = officeNumber
+    }
+
+    getRole(){
+        return "Manager"
+    }
+
+}
+
+
+
