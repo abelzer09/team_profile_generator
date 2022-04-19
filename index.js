d803b5b3ef16f29b81fa6a9badc0b7fec855027d
abelzer09 <aaronrbelzer@gmail.com>
2022-04-19 16:21:28 -0500

    created employee.js file in lib folder, copied employee class and moved to that file

--- index.js
+++ index.js
@@ -3,30 +3,6 @@ const fs = require('fs');
 const path = require('path');
 
 
-class Employee {
-    constructor(name, id, email){
-        this.name = name;
-        this.id = id;
-        this.email = email;
-    }
-    
-    getName(){
-
-    }
-
-    getId(){
-
-    }
-
-    getEmail(){
-
-    }
-
-    getRole(){
-        return "Employee"
-    }
-}
-
 class Manager extends Employee {
     constructor(name, officeNumber){
         super(name, id, email);
