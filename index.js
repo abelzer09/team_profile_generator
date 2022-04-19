b1292570ba42f4c2f29af7a74618c8af104292fa
abelzer09 <aaronrbelzer@gmail.com>
2022-04-19 16:26:48 -0500

    created intern.js and engineer.js files in lib folder, copied intern and engineer to .js files

--- index.js
+++ index.js
@@ -2,34 +2,3 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const path = require('path');
 
-
-class Engineer extends Employee {
-    constructor(name, github){
-        super(name, id, email);
-        this.github = github;
-    }
-
-    getGithub(){
-
-    }
-
-    getRole(){
-        return "Engineer"
-    }
-}
-
-class Intern extends Employee {
-    constructor(name, school) {
-        super(name, school);
-        this.school = school;
-    }
-    
-    getSchool(){
-
-    }
-
-    getRole(){
-        return "Intern"
-    }
-}
-
