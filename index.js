d19c1d65582b2346a4d3e49360a8076dd1fc2767
abelzer09 <aaronrbelzer@gmail.com>
2022-04-19 16:46:26 -0500

    added questions for inquirer prompt to create manager role

--- index.js
+++ index.js
@@ -2,3 +2,32 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const path = require('path');
 
+const questions = [
+    {
+        type: "input",
+        name: 'name',
+        message: 'Enter Managers Name.',
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: "Enter Employee ID Number.",
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: "Enter email address.",
+    },
+    {
+        type: 'input',
+        name: 'office',
+        message: "Enter office number.",
+    },
+    {
+        type: "list",
+        name: 'team',
+        message: "Would you like to add any additional team members",
+        choices: ['Engineer', 'Intern', 'Finish building team'],
+    },
+]
+
