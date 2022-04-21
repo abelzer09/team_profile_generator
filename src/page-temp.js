9a0c9db1a7c8ef7a34f3093080df2eb949d08fe4
abelzer09 <aaronrbelzer@gmail.com>
2022-04-20 19:22:44 -0500

    added css styling and further html improvments

--- src/page-temp.js
+++ src/page-temp.js
@@ -3,9 +3,9 @@ const generate = team =>{
         return `
         <div class="col">
           <div class="card">
-            <div class="card-body">
-              <h5 class="card-title">${manager.getName()}</h5>
-              <h6 class="card-title">Manager</h6>
+            <div class="card-body title1">
+              <h4 class="card-title">${manager.getName()}</h4>
+              <h5 class="card-title">Manager</h5>
             </div>
             <ul class="list-group list-group-flush">
                 <li class="list-group-item">ID: ${manager.getID()}</li>
@@ -19,9 +19,9 @@ const generate = team =>{
         return `
         <div class="col">
           <div class="card">
-            <div class="card-body">
-              <h5 class="card-title">${engineer.getName()}</h5>
-              <h6 class="card-title">Engineer</h6>
+            <div class="card-body title1">
+              <h4 class="card-title">${engineer.getName()}</h4>
+              <h5 class="card-title">Engineer</h5>
             </div>
             <ul class="list-group list-group-flush">
                 <li class="list-group-item">ID: ${engineer.getID()}</li>
@@ -35,9 +35,9 @@ const generate = team =>{
         return `
         <div class="col">
         <div class="card">
-          <div class="card-body">
-            <h5 class="card-title">${intern.getName()}</h5>
-            <h6 class="card-title">Intern</h6>
+          <div class="card-body title1">
+            <h4 class="card-title">${intern.getName()}</h4>
+            <h5 class="card-title">Intern</h5>
           </div>
           <ul class="list-group list-group-flush">
               <li class="list-group-item">ID: ${intern.getID()}</li>
@@ -75,11 +75,19 @@ module.exports = team =>{
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="./style.css"> 
+    <title>Team Profile</title>
 </head>
 <body>
+    <h1>My Team</h1>
 
-    ${generate(team)}
+    <div class="container">
+      <div class="row align-items-center row-cols-1 row-cols-md-3 g-4">
+        ${generate(team)}
+      </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
 </html>`
 }
